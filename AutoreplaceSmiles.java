b0885726501ca6ba832a58519bf78dac2067a1a6
Jangwoo Lee <jangwoo24@gmail.com>
2019-05-28 20:44:16 -0700

    Add heart emoji

--- AutoreplaceSmiles.java
+++ AutoreplaceSmiles.java
@@ -4,12 +4,16 @@ import javax.swing.event.DocumentListener;
 import javax.swing.text.*;
 import java.awt.image.BufferedImage;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
  
-public class AutoreplaceSmiles extends JEditorPane {
-    String[] emoticons = {":)", ":("};
-    static ImageIcon SMILE_IMG = createImage(":)");
+public class AutoreplaceSmiles extends JTextPane {
+    String[] emoticons = {":)", ":D", ":(", ">:(", "<3"};
+    /*static ImageIcon SMILE_IMG = createImage(":)");
+    static ImageIcon ANGER_IMG = createImage(">:(");
     static ImageIcon FROWN_IMG = createImage(":(");
-    static ImageIcon[] emojis = {SMILE_IMG, FROWN_IMG};
+    static ImageIcon[] emojis = {SMILE_IMG, ANGER_IMG, FROWN_IMG};*/
+    static List<ImageIcon> emojis = new ArrayList<>();
  
     public static void main(String[] args) {
         JFrame frame = new JFrame("Autoreplace :) with Smiles images example");
@@ -27,6 +31,9 @@ public class AutoreplaceSmiles extends JEditorPane {
  
     public AutoreplaceSmiles() {
         super();
+        for(String emote: emoticons) {
+        	emojis.add(createImage(emote));
+        }
     }
  
     private void initListener() {
@@ -48,11 +55,11 @@ public class AutoreplaceSmiles extends JEditorPane {
 	                                    final SimpleAttributeSet attrs=new SimpleAttributeSet(
 	                                       doc.getCharacterElement(start+i).getAttributes());
 	                                    if (StyleConstants.getIcon(attrs)==null) {
-	                                        StyleConstants.setIcon(attrs, emojis[index]);
-	                                        doc.remove(start+i, 2);
-	                                        doc.insertString(start+i,":)", attrs);
+	                                        StyleConstants.setIcon(attrs, emojis.get(index));
+	                                        doc.remove(start+i, emoticons[index].length());
+	                                        doc.insertString(start+i,emoticons[index], attrs);
 	                                    }
-	                                    i=text.indexOf(":)", i+2);
+	                                    i=text.indexOf(emoticons[index], i+emoticons[index].length());
 	                                }
 	                            }
                             } catch (BadLocationException e1) {
@@ -89,6 +96,22 @@ public class AutoreplaceSmiles extends JEditorPane {
 	        // g.drawLine(4,10, 8,12);
 	        // g.drawLine(8,12, 12,10);
 	        g.drawArc(3,3,11,10,225,90);
+	    } else if(":D".equals(icon)) {//open smile
+	    	g.setColor(Color.yellow);
+	        g.fillOval(0,0,16,16);
+	 
+	        g.setColor(Color.black);
+	        g.drawOval(0,0,16,16);
+	 
+	        g.drawLine(4,5, 6,5);//left eye
+	        g.drawLine(4,6, 6,6);
+	 
+	        g.drawLine(12,5, 10,5);//right eye
+	        g.drawLine(12,6, 10,6);
+
+	        g.fillArc(3,4,11,10,180,180);
+	        //g.drawArc(3,4,11,10,180,180);
+	        //g.drawLine(3,11, 14,11);
 	    } else if(":(".equals(icon)) {//simple frown
 	    	g.setColor(Color.yellow);
 	        g.fillOval(0,0,16,16);
@@ -105,6 +128,69 @@ public class AutoreplaceSmiles extends JEditorPane {
 	        // g.drawLine(4,12, 8,10);//mouth
 	        // g.drawLine(8,10, 12,12);
 	        g.drawArc(3,11,11,10,45,90);
+	    } else if(">:(".equals(icon)) {//angry frown
+	    	g.setColor(Color.red);
+	        g.fillOval(0,0,16,16);
+	 
+	        g.setColor(Color.black);
+	        g.drawOval(0,0,16,16);
+	 
+	        g.drawLine(4,5, 5,5);//left eye
+	        g.drawLine(4,6, 6,6);
+	 
+	        g.drawLine(12,5, 11,5);//right eye
+	        g.drawLine(12,6, 10,6);
+	 
+	        g.drawLine(7,9, 11,9); //mouth
+	        g.drawLine(7,14, 11,14);
+
+	        g.drawLine(5,10, 6,10);
+	        g.drawLine(5,13, 6,13);
+	        g.drawLine(10,10, 11,10);
+	        g.drawLine(10,13, 11,13);
+
+	        g.drawLine(4,11, 4,12);
+	        g.drawLine(12,11, 12,12);
+
+	        g.drawLine(6,11, 6,13);
+	        g.drawLine(8,11, 8,13);
+	        g.drawLine(10,11, 10, 13);
+
+	        g.setColor(Color.white);
+	        g.drawLine(5,11, 5,12);
+	        g.drawLine(7,9, 7,13);
+	        g.drawLine(9,9, 9,13);
+	        g.drawLine(11,11, 11,12);
+	    } else if("<3".equals(icon)) {//heart
+	    	g.setColor(Color.black);
+	    	g.drawLine(8,15, 15,8);
+	    	g.drawLine(15,8, 15,5);
+	    	g.drawLine(15,5, 12,2);
+	    	g.drawLine(12,2, 10,2);
+	    	g.drawLine(10,2, 8,4);
+	    	g.drawLine(8,4, 6,2);
+	    	g.drawLine(6,2, 4,2);
+	    	g.drawLine(4,2, 1,5);
+	    	g.drawLine(1,5, 1,8);
+	    	g.drawLine(1,8, 8,15);
+
+	    	g.setColor(Color.red);
+	    	g.drawLine(2,7, 2,5);
+	    	g.drawLine(2,5, 3,5);
+	    	g.drawLine(4,3, 6,3);
+	    	g.drawLine(3,4, 7,4);
+	    	g.drawLine(5,5, 14,5);
+	    	g.drawLine(4,6, 14,6);
+	    	g.drawLine(4,7, 14,7);
+	    	g.drawLine(2,8, 14,8);
+	    	g.drawLine(3,9, 13,9);
+	    	g.drawLine(4,10, 12,10);
+	    	g.drawLine(5,11, 11,11);
+	    	g.drawLine(6,12, 10,12);
+	    	g.drawLine(7,13, 9,13);
+	    	g.drawLine(8,14, 8,14);
+	    	g.drawLine(9,4, 13,4);
+	    	g.drawLine(10,3, 12,3);
 	    }
         g.dispose();
  
